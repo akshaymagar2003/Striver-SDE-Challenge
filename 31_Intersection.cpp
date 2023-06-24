e4d0276ba39af05bcce4a7767dcb53c8ee3e9420
Akshay Magar <akshay88magar@gmail.com>
2023-06-24 18:05:37 +0530

    Question 31

---
+++ 31_Intersection.cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+/****************************************************************
+
+    Following is the class structure of the Node class:
+
+        class Node
+        {
+        public:
+            int data;
+            Node *next;
+            Node()
+            {
+                this->data = 0;
+                next = NULL;
+            }
+            Node(int data)
+            {
+                this->data = data;
+                this->next = NULL;
+            }
+            Node(int data, Node* next)
+            {
+                this->data = data;
+                this->next = next;
+            }
+        };
+
+*****************************************************************/
+
+Node* findIntersection(Node *firstHead, Node *secondHead)
+{
+   
+    //Write your code here
+
+    Node *f=firstHead;
+
+    Node *s=secondHead;
+
+    unordered_set<Node*>st;
+
+     while(f)
+
+     {
+
+         st.insert(f);
+
+         f=f->next;
+
+ 
+
+     }
+
+     while(s)
+
+     {
+
+         if(st.find(s)!=st.end())
+
+         {
+
+             return s;
+
+         }
+
+         s=s->next;
+
+     }
+
+    
+
+    return NULL;
+}
